8939c030e7dd500448259215efb403e4605d9dbd
StiansbD <chris33et24@gmail.com>
2019-03-26 18:02:30 +0100

    Test pour la vue d'une page de redirection.

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,40 +1,13 @@
 <!DOCTYPE html>
 <html>
-<head><title>Laravel</title>
-  <style media="screen">
-    html, body {
-      height: 100%;
-    }
-
-    body {
-      margin: 0;
-      padding: 0;
-      width: 100%;
-      display: table;
-      font-weight: 100;
-      font-size: 20px;
-      font-family: 'Helvetica';
-    }
-
-    .container {
-      text-align: center;
-      display: table-cell;
-      vertical-align: middle;
-    }
-
-    .content {
-      text-align: center;
-      display: inline-block;
-    }
-
-    .title {
-      font-size: 110px;
-    }
-  </style>
+<head><title>GIE</title>
+  <link rel="stylesheet" href="/css/home.css">
+  <link rel="icon" type="image/png" href="/css/img/logo.png" />
 </head>
 <body>
   <div class='container'>
-    <div class='content'>
+      <div class='content'>
+        <img src="/css/img/logo.png" alt="logo du GIE">
       <div class='title'>Bienvenue sur le site du GIE !</div>
       <div class='container'>
       <div class='content'>Bienvenue sur le nouveau site du GIE. Il est en cours de construction mais il sera bientôt disponible. Cependant si vous souhaitez accéder à l'ancienne version je vous invite à <a href="https://gie.polygames.net/">cliquer ici</a>.
