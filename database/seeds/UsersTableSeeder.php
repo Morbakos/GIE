b7019612a2429e94e63c0af2786b2dfb35bb0621
GIE <contact@team-gie.com>
2019-07-25 15:49:57 +0200

    MaJ affichage graphique

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -11,8 +11,140 @@ class UsersTableSeeder extends Seeder {
         DB::table('users')->insert([
             'id_user' => 1,
             'user_pseudo' => 'Morbakos',
-            'user_mdp' => bcrypt('monmdp'),
+            'user_mdp' => bcrypt('GIEMorbakos1'),
             'email' => 'morbakos@team-gie.com',
+            'user_avatar' => '',
+            'user_valide' => 1,
+            'user_rang' => 4,
+            'user_post' => 0
+        ]);
+		
+		DB::table('users')->insert([
+            'id_user' => 2,
+            'user_pseudo' => 'Pippermint',
+            'user_mdp' => bcrypt('GIEPippermint2'),
+            'email' => 'pippermint@team-gie.com',
+            'user_avatar' => '',
+            'user_valide' => 1,
+            'user_rang' => 4,
+            'user_post' => 0
+        ]);
+		
+		DB::table('users')->insert([
+            'id_user' => 3,
+            'user_pseudo' => 'LeDoc',
+            'user_mdp' => bcrypt('GIELeDoc3'),
+            'email' => 'ledoc@team-gie.com',
+            'user_avatar' => '',
+            'user_valide' => 1,
+            'user_rang' => 4,
+            'user_post' => 0
+        ]);
+		
+		DB::table('users')->insert([
+            'id_user' => 4,
+            'user_pseudo' => 'Kaddosh',
+            'user_mdp' => bcrypt('GIEKaddosh4'),
+            'email' => 'kaddosh@team-gie.com',
+            'user_avatar' => '',
+            'user_valide' => 1,
+            'user_rang' => 4,
+            'user_post' => 0
+        ]);
+		
+		DB::table('users')->insert([
+            'id_user' => 5,
+            'user_pseudo' => 'Jack Smith',
+            'user_mdp' => bcrypt('GIEJackS5'),
+            'email' => 'jack.smith@team-gie.com',
+            'user_avatar' => '',
+            'user_valide' => 1,
+            'user_rang' => 4,
+            'user_post' => 0
+        ]);
+		
+		DB::table('users')->insert([
+            'id_user' => 6,
+            'user_pseudo' => 'Stians',
+            'user_mdp' => bcrypt('GIEStians6'),
+            'email' => 'stians@team-gie.com',
+            'user_avatar' => '',
+            'user_valide' => 1,
+            'user_rang' => 4,
+            'user_post' => 0
+        ]);
+		
+		DB::table('users')->insert([
+            'id_user' => 7,
+            'user_pseudo' => 'Seemann',
+            'user_mdp' => bcrypt('GIESeemann7'),
+            'email' => 'seemann@team-gie.com',
+            'user_avatar' => '',
+            'user_valide' => 1,
+            'user_rang' => 4,
+            'user_post' => 0
+        ]);
+		
+		DB::table('users')->insert([
+            'id_user' => 8,
+            'user_pseudo' => 'Amau',
+            'user_mdp' => bcrypt('GIEAmau8'),
+            'email' => 'amau@team-gie.com',
+            'user_avatar' => '',
+            'user_valide' => 1,
+            'user_rang' => 4,
+            'user_post' => 0
+        ]);
+		
+		DB::table('users')->insert([
+            'id_user' => 9,
+            'user_pseudo' => 'Xoranoj',
+            'user_mdp' => bcrypt('GIEXoranoj9'),
+            'email' => 'xoranoj@team-gie.com',
+            'user_avatar' => '',
+            'user_valide' => 1,
+            'user_rang' => 4,
+            'user_post' => 0
+        ]);
+		
+		DB::table('users')->insert([
+            'id_user' => 10,
+            'user_pseudo' => 'Madness',
+            'user_mdp' => bcrypt('GIEMadness10'),
+            'email' => 'madness@team-gie.com',
+            'user_avatar' => '',
+            'user_valide' => 1,
+            'user_rang' => 4,
+            'user_post' => 0
+        ]);
+		
+		DB::table('users')->insert([
+            'id_user' => 11,
+            'user_pseudo' => 'Kathlyes',
+            'user_mdp' => bcrypt('GIEKathlyes11'),
+            'email' => 'kathlyes@team-gie.com',
+            'user_avatar' => '',
+            'user_valide' => 1,
+            'user_rang' => 4,
+            'user_post' => 0
+        ]);
+		
+		DB::table('users')->insert([
+            'id_user' => 12,
+            'user_pseudo' => 'Tisma',
+            'user_mdp' => bcrypt('GIETisma12'),
+            'email' => 'tisma@team-gie.com',
+            'user_avatar' => '',
+            'user_valide' => 1,
+            'user_rang' => 4,
+            'user_post' => 0
+        ]);
+		
+		DB::table('users')->insert([
+            'id_user' => 13,
+            'user_pseudo' => 'Baal',
+            'user_mdp' => bcrypt('GIEBaal13'),
+            'email' => 'baal@team-gie.com',
             'user_avatar' => '',
             'user_valide' => 1,
             'user_rang' => 4,
--- resources/views/missions/index.blade.php
+++ resources/views/missions/index.blade.php
@@ -12,6 +12,7 @@
 
    <body>
    <div class='container'>
+   {!! $links !!}
    <div class="tableau">
    <div class='title'>Nos missions </div><br/>
       <table border = 1 id="tableau" class='center' style="text-align:center">
@@ -53,7 +54,7 @@
          </tr>
          @endforeach
       </table>
-      </div>
+	  </div>
       <br/><br/><br/><br/>
       @if (Auth::check())
       {!! Form::open(['route' => 'missions.store', 'class' => 'form-horizontal panel']) !!}
@@ -70,6 +71,8 @@
 
           {!! Form::text('auteur_mission', null, ['class' => 'form-control', 'placeholder' => 'Auteur(s) de la mission']) !!}<br/><br/>
           {!! $errors->first('auteur_mission', '<small class="help-block">:message</small>') !!}
+		  </div>
+		  </div>
 
 
           <div class="form-group {!! $errors->has('map_mission') ? 'has-error' : '' !!}">
@@ -78,9 +81,11 @@
             'Map de CUP' => array('Bukovina' => 'Bukovina', 'Bystrica' => 'Bystrica', 'Chernarus (Autumn)' => 'Chernarus (Autumn)', 'Chernarus (Summer)' => 'Chernarus (Summer)', 'Chernarus (Winter)' => 'Chernarus (Winter)', 'Desert' => 'Desert','Desert Island' => 'Desert Island','Everon' => 'Everon','Kolgujev' => 'Kolgujev','Malden' => 'Malden','Nogova' => 'Nogova','Porto' => 'Porto','Proving Grounds' => 'Proving Grounds','Rahmadi' => 'Rahmadi','Sahrani' => 'Sahrani','Shapur' => 'Shapur','Southern Sahrani' => 'Southern Sahrani','Takistan' => 'Takistan','Takistan_mountains' => 'Takistan Mountains','United_sahrani' => 'United Sahrani','Utes' => 'Utes','Zargabad' => 'Zargabad'),
           ));!!}<br/><br/>
           {!! $errors->first('map_mission', '<small class="help-block">:message</small>') !!}
+		  </div>
 
           <div class="form-group {!! $errors->has('composante_mission') ? 'has-error' : '' !!}">
           Composantes:<br>
+		  </div>
           <div class="composante-mission">
           {!! Form::checkbox('composante_mission[]', 'Alpha') !!}Alpha<br>
           {!! Form::checkbox('composante_mission[]', 'Bravo') !!}Bravo<br>
@@ -96,40 +101,47 @@
           <div class="form-group {!! $errors->has('nombre_slots_mission') ? 'has-error' : '' !!}">
           Nbr Slots :<br>&nbsp&nbsp&nbsp&nbsp{!! Form::text('nombre_slots_mission', null, ['class' => 'form-control', 'placeholder' => 'Nombre de slots'])!!}<br/><br/>
           {!! $errors->first('nombre_slots_mission', '<small class="help-block">:message</small>') !!}
+		  </div>
 
           <div class="form-group {!! $errors->has('correction_mission') ? 'has-error' : '' !!}">
           Correction :<br>&nbsp&nbsp&nbsp&nbsp{!! Form::text('correction_mission', null, ['class' => 'form-control', 'placeholder' => 'Correction nécessaire'])!!}<br/><br/>
           {!! $errors->first('correction_mission', '<small class="help-block">:message</small>') !!}
+		  </div>
 
           <div class="form-group {!! $errors->has('zeus_mission') ? 'has-error' : '' !!}">
           Zeus nécessaire ?&nbsp&nbsp&nbsp&nbspOui {!! Form::radio('zeus_mission', 'Oui')!!} - Non
           {!! Form::radio('zeus_mission', 'Non', true)!!}<br/><br/>
           {!! $errors->first('zeus_mission', '<small class="help-block">:message</small>') !!}
+		  </div>
 
           <div class="form-group {!! $errors->has('briefing_mission') ? 'has-error' : '' !!}">
           Briefing :<br>&nbsp&nbsp&nbsp&nbsp{!! Form::textarea('briefing_mission', null, ['class' => 'form-control', 'placeholder' => 'Briefing à saisir']) !!} <br/><br/>
           {!! $errors->first('briefing_mission', '<small class="help-block">:message</small>') !!}
+		  </div>
 
 
           <div class="form-group {!! $errors->has('hostile_mission') ? 'has-error' : '' !!}">
           Ennemis :<br>&nbsp&nbsp&nbsp&nbsp{!! Form::text('hostile_mission', null, ['class' => 'form-control', 'placeholder' => 'Hostile de la mission']) !!}<br/><br/>
           {!! $errors->first('hostile_mission', '<small class="help-block">:message</small>') !!}
+		  </div>
 
 
           <div class="form-group {!! $errors->has('duree_estimee_mission') ? 'has-error' : '' !!}">
           Durée estimée :<br>&nbsp&nbsp&nbsp&nbsp{!! Form::text('duree_estimee_mission', null, ['class' => 'form-control', 'placeholder' => 'Durée estimée']) !!}<br/><br/>
           {!! $errors->first('duree_estimee_mission', '<small class="help-block">:message</small>') !!}
+		  </div>
 
           <div class="form-group {!! $errors->has('statut_mission') ? 'has-error' : '' !!}">
           Statut de la mission :<br>&nbsp&nbsp&nbsp&nbsp{!! Form::select('statut_mission', array(
             'Statut actuel :' => array('A tester' => 'A tester','Jouable' => 'Jouable', 'A corriger' => 'A corriger'),
           ));!!}<br/><br/>
           {!! $errors->first('statut', '<small class="help-block">:message</small>') !!}
-
+		  </div>
 
           <div class="form-group {!! $errors->has('nombre_jouer_mission') ? 'has-error' : '' !!}">
           Nombre de fois jouer :<br>&nbsp&nbsp&nbsp&nbsp{!! Form::text('nombre_jouer_mission', null, ['class' => 'form-control', 'placeholder' => 'Nombre de fois jouer']) !!}<br/><br/>
           {!! $errors->first('nombre_jouer_mission', '<small class="help-block">:message</small>') !!}
+		  </div>
 
           <div class="form-group {!! $errors->has('type_mission') ? 'has-error' : '' !!}">
           Type de mission :<br>&nbsp&nbsp&nbsp&nbsp{!! Form::select('type_mission', array(
@@ -144,7 +156,5 @@
 
         {!! Form::close() !!}
         @endif
-        </div>
-      {!! $links !!}
 </body>
 </html>
